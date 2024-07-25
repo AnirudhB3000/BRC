c09abfe332890e77ec3ec0fe1609afd85aa8054c
anirudhb3000 <anirudhbhattacharya1@gmail.com>
2024-07-25 12:15:45 -0700

    Created modules

---
+++ NaiveMethod.h
@@ -0,0 +1,90 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include "Result.h"
+
+class NaiveMethod {
+    std::ifstream fh;
+    std::vector<Result> results;
+
+    int getCityIndex(const std::string &city, const std::vector<Result> &results) const {
+        /*
+        Args:
+        city: string of city
+        results: required data
+
+        Functionality:
+        Helper function for naiveMethod, returns the index of the city.
+        */
+        for (size_t i = 0; i < results.size(); ++i) {
+            if (results[i].city == city) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static bool compareResults(const Result &a, const Result &b) {
+        /*
+        Args:
+        a, b: cities
+
+        Functionality:
+        Helper function for naiveMethod
+        */
+        return a.city < b.city;
+    }
+
+public:
+    NaiveMethod(const std::string &file) {
+        fh.open(file);
+        if (!fh) {
+            std::cerr << "\nError opening file" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+    }
+
+    void process() {
+        /*
+        Args:
+        fh: the file scanned from disk
+        results: the output vector
+        
+        Functionality:
+        Modified the vector results to the required output
+        */
+        std::string line;
+        while (std::getline(fh, line)) {
+            size_t pos = line.find(';');
+            if (pos == std::string::npos) continue;
+
+            std::string city = line.substr(0, pos);
+            double measurement = std::stod(line.substr(pos + 1));
+
+            int c = getCityIndex(city, results);
+            if (c < 0) {
+                Result res = { city, 1, measurement, measurement, measurement };
+                results.push_back(res);
+            } else {
+                results[c].sum += measurement;
+                results[c].count += 1;
+                if (results[c].min > measurement) {
+                    results[c].min = measurement;
+                }
+                if (results[c].max < measurement) {
+                    results[c].max = measurement;
+                }
+            }
+        }
+
+        std::sort(results.begin(), results.end(), compareResults);
+    }
+
+    const std::vector<Result>& getResults() const {
+        return results;
+    }
+};
---
+++ Result.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+struct Result {
+    std::string city;
+    int count;
+    double sum, min, max;
+};
